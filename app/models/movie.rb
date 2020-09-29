484485ef3f1e95859c921992614c8147630cdc07
Joseph O'Neill <joneill1@citadel.edu>
2020-09-29 04:49:57 +0000

    finished part 2

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,15 @@
 class Movie < ActiveRecord::Base
+    
+    def self.all_ratings
+        ['G','PG','PG-13','R']
+    end
+    
+    def self.all_ratings_as_hash
+        {'G'=>'G','PG'=>'PG','PG-13'=>'PG-13','R'=>'R'}
+    end
+    
+    def self.with_ratings ratings
+        Movie.where(rating: ratings)
+    end
+    
 end
